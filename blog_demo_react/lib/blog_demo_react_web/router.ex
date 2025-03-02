8d3c2db7e1f888a7cdcd8d20f44d8b62f1e2c446
Alex Kuang <alex@bikeshed.coffee>
2025-03-02 17:31:17 -0500

    server-side install instructions from inertia readme

--- blog_demo_react/lib/blog_demo_react_web/router.ex
+++ blog_demo_react/lib/blog_demo_react_web/router.ex
@@ -8,6 +8,8 @@ defmodule BlogDemoReactWeb.Router do
     plug :put_root_layout, html: {BlogDemoReactWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+
+    plug Inertia.Plug
   end
 
   pipeline :api do
