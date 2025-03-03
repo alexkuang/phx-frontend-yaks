acaef8e740d821d05ed8d4b6e3eb45ab88499ee3
Alex Kuang <alex@bikeshed.coffee>
2025-03-03 05:56:18 -0500

    install live_state

--- blog_demo_react/lib/blog_demo_react_web/endpoint.ex
+++ blog_demo_react/lib/blog_demo_react_web/endpoint.ex
@@ -1,6 +1,8 @@
 defmodule BlogDemoReactWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :blog_demo_react
 
+  plug CORSPlug
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
